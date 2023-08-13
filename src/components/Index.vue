5e310d8c4d40e503d9ea3a3b6505e54189c6e520
izyer <izyeeclipse@gmail.com>
2023-08-13 18:03:05 +0200

    Changing status + better responsiveness

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="typing_word">
     <p class="big_text">Je travaille avec,
-    <vue-writer :array="['Java','Ruby','Kotlin','PHP', 'C#','Intellij IDEA']" :typeSpeed="70" />
+      <vue-writer :array="['Java','Ruby','Kotlin','PHP', 'C#','Intellij IDEA']" :typeSpeed="70"/>
     </p>
   </section>
 
@@ -10,19 +10,27 @@
     <img src="../assets/back_img.png" alt="back_img" id="back-image" class="unselectable" draggable="false">
     <h1 class="common-section-title">Qui suis-je ?</h1>
     <p class="common-section-paragraph" id="index-common-section">
-      Actuellement en <span style="color: #bb6565"> BTS SIO (Service informatique aux organisations) </span>, je suis depuis longtemps, passionné par l'informatique notamment dans le domaine du <span style="color: #89a969">développement d'application</span> / <span style="color: #89a969">jeux-vidéos</span>.
+      Actuellement en <span style="color: #bb6565"> Bachelor CSI (Concepteur des systèmes d'informations) </span>, je
+      suis depuis longtemps, passionné par l'informatique notamment dans le domaine du <span style="color: #89a969">développement d'application</span>
+      / <span style="color: #89a969">jeux-vidéos</span>.
     </p>
-    <img src="../assets/exp.png" alt="illus1" id = "illus1" class="unselectable" draggable="false">
+    <img src="../assets/exp.png" alt="illus1" id="illus1" class="unselectable" draggable="false">
     <p class="common-section-paragraph" id="index-common-section">
-      J'ai découvert le monde du développement grâce au jeu-vidéo Minecraft. J'ai débuté avec de simples plugins puis au fils des années j'ai pu faire évoluer ma façon de coder et parfaire ma maîtrise du langage pour réaliser des programmes plus complexes comme des mini-jeux ou des API de gestion de réseau.
+      J'ai découvert le monde du développement grâce au jeu-vidéo Minecraft. J'ai débuté avec de simples plugins puis au
+      fils des années j'ai pu faire évoluer ma façon de coder et parfaire ma maîtrise du langage pour réaliser des
+      programmes plus complexes comme des mini-jeux ou des API de gestion de réseau.
     </p>
 
     <p class="common-section-paragraph" id="index-common-section">
-      Je travaille désormais essentiellement avec les langages <span style="color: #fa9e9e">Java</span>, <span style="color: #ff4242">Ruby</span>, <span style="color: #fd9cf7">Kotlin</span>, <span style="color: #caf360">C#</span> et <span style="color: #75b1ff">PHP</span>.
+      Je travaille désormais essentiellement avec les langages <span style="color: #fa9e9e">Java</span>, <span
+        style="color: #ff4242">Ruby</span>, <span style="color: #fd9cf7">Kotlin</span>, <span
+        style="color: #caf360">C#</span> et <span style="color: #75b1ff">PHP</span>.
     </p>
 
     <h3 class="common-section-low-title">PROJETS</h3>
-    <p class="common-section-paragraph" id="index-common-section">J'ai eu l'occasion de travailler sur divers projet, <span style="color: #6987a9">seul</span> ou en <span style="color: #6989a9">équipe</span>. Il est notamment possible de retrouver quelque uns de ces projets sur mon <span style="color: #ce6868">github</span>.</p>
+    <p class="common-section-paragraph" id="index-common-section">J'ai eu l'occasion de travailler sur divers projet,
+      <span style="color: #6987a9">seul</span> ou en <span style="color: #6989a9">équipe</span>. Il est notamment
+      possible de retrouver quelque uns de ces projets sur mon <span style="color: #ce6868">github</span>.</p>
     <div id="learn-more-div">
       <router-link to="/projects" id="router-link">En savoir plus</router-link>
     </div>
@@ -33,7 +41,7 @@
 import VueWriter from 'vue-writer'
 
 export default {
-  components: { VueWriter },
+  components: {VueWriter},
   name: "index_page"
 }
 </script>
